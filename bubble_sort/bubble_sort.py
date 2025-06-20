fc63f76eb40be724c87038d6689f4080ded4287b
Gabriel Osugo Kabese <gabriel.kabese@strathmore.edu>
2025-06-20 09:34:50 +0300

    Class assignment

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if unsorted_list[j] > unsorted_list[j+1]:
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+    return unsorted_list
+
+input_list = [4, 5, 3, 4]
+sorted_list = bubble_sort(input_list)
+print("Output:", sorted_list)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    n = len(arr)
+
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j]<arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+input_list = [29, 10, 14, 37, 14]
+sorted_list = selection_sort(input_list)
+print("Expected output:", sorted_list)
